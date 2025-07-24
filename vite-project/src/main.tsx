425035562272b787b7ab41df751c83960fbf18ab
Ben <130092942+BennDiagram@users.noreply.github.com>
2025-07-24 03:39:51 -0400

    Second carousel built

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Carousel from "./carousel";
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "../node_modules/swiper/swiper-bundle.css";
 
 const HomePage: React.FC = () => (
   <div className="max-w-2xl mx-auto mt-10">
@@ -16,3 +20,37 @@ if (rootElement) {
     </React.StrictMode>
   );
 }
+
+//initialize Swiper
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  effect: "coverflow",
+  coverflowEffect: {
+    depth: 1,
+    slideShadows: false,
+    rotate: 0,
+    scale: 0.9,
+  },
+
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
