6c53267c29bebe39091df028f5e2ac4debd216b7
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-07-29 14:00:42 -0400

    Background blend works now
    It is wonky with the way it works - might be beneficial to add these to index.css or possibly make a component(?) for a blending background

--- Haven/src/components/Events/Events.tsx
+++ Haven/src/components/Events/Events.tsx
@@ -3,7 +3,7 @@ import events from "./events-list";
 import banner1 from "../../assets/media/banners/banner1.jpg";
 import TitleContainer from "../TitleContainer/TitleContainer";
 import EventItem from "../EventItem/EventItem";
-import bottom1 from "../../assets/media/backgrounds/bottom1.png";
+import bottom1 from "../../assets/media/banners/bottom1.jpg";
 
 export default function UpcomingEvents() {
   return (
@@ -11,18 +11,20 @@ export default function UpcomingEvents() {
       <TitleContainer text="EVENTS" image={banner1} />
       <div className="events-list-container">
         <img src={bottom1} alt="" className="bg-image" />
-        {events.map((event, index) => {
-          return (
-            <EventItem
-              title={event.title}
-              date={event.date}
-              doorsOpen={event.doorsOpen}
-              price={event.price}
-              link={event.link}
-              image={event.image}
-            />
-          );
-        })}
+        <div className="event-items-wrapper">
+          {events.map((event, index) => {
+            return (
+              <EventItem
+                title={event.title}
+                date={event.date}
+                doorsOpen={event.doorsOpen}
+                price={event.price}
+                link={event.link}
+                image={event.image}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- Haven/src/components/WhoWeAre/WhoWeAre.tsx
+++ Haven/src/components/WhoWeAre/WhoWeAre.tsx
@@ -1,7 +1,7 @@
 import ButtonTertiary from "../Button/ButtonTertiary";
 import "./who-we-are.css";
 import banner1 from "../../assets/media/banners/banner1.jpg";
-import bottom1 from "../../assets/media/backgrounds/bottom1.png";
+import bottom1 from "../../assets/media/banners/bottom1.jpg";
 
 import TitleContainer from "../TitleContainer/TitleContainer";
 
@@ -11,29 +11,34 @@ export default function WhoWeAre() {
       <TitleContainer image={banner1} text="WHO WE ARE" />
       <div className="body-container">
         <img src={bottom1} alt="" className="bg-image" />
-        <p>
-          <span className="content-header"> HAVEN </span> is an independent
-          music venue in the heart of Pittsburgh’s Oakland neighborhood. The
-          venue is operated by{" "}
-          <a href="https://pg.media" target="_blank">
-            <u>
-              <i>Post Genre,</i>
-            </u>
-          </a>{" "}
-          a Pittsburgh-based organization dedicated to enriching the local
-          community through concerts, event production training, and more.
-        </p>
-        <ButtonTertiary link="google.com" text="LEARN MORE ABOUT POST GENRE" />
-        <p>
-          <span className="content-header"> WHY HAVEN?</span>
-        </p>
-        <p>
-          Once the heart of music in Pittsburgh, Oakland’s vibrant music scene
-          was pushed underground after a wave of venue closures in the early
-          2000’s. Although these venues left Oakland, the borough’s passion for
-          local music persevered.
-        </p>
-        <ButtonTertiary link="google.com" text="HAVEN FAQ" />
+        <div className="body-text-container">
+          <p>
+            <span className="content-header"> HAVEN </span> is an independent
+            music venue in the heart of Pittsburgh’s Oakland neighborhood. The
+            venue is operated by{" "}
+            <a href="https://pg.media" target="_blank">
+              <u>
+                <i>Post Genre,</i>
+              </u>
+            </a>{" "}
+            a Pittsburgh-based organization dedicated to enriching the local
+            community through concerts, event production training, and more.
+          </p>
+          <ButtonTertiary
+            link="google.com"
+            text="LEARN MORE ABOUT POST GENRE"
+          />
+          <p>
+            <span className="content-header"> WHY HAVEN?</span>
+          </p>
+          <p>
+            Once the heart of music in Pittsburgh, Oakland’s vibrant music scene
+            was pushed underground after a wave of venue closures in the early
+            2000’s. Although these venues left Oakland, the borough’s passion
+            for local music persevered.
+          </p>
+          <ButtonTertiary link="google.com" text="HAVEN FAQ" />
+        </div>
       </div>
     </div>
   );
