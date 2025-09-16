3ee18c387b92d95dbdbeadc0d8d3a3f56bc6bc90
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-09-16 12:48:25 -0400

    added integration with IndieTix events
    pagination is a little buggy with the coverflow effect

--- Haven/src/components/Carousel/Carousel.tsx
+++ Haven/src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 import "./carousel.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CarouselItem from "../CarouselItem/CarouselItem";
 import { FaAngleLeft, FaAngleRight, FaCircle } from "react-icons/fa6";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -18,15 +18,88 @@ import "swiper/css/pagination";
 export default function Carousel() {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const carouselItems = [
-    <CarouselItem index={0} />,
-    <CarouselItem index={1} />,
-    <CarouselItem index={2} />,
-    <CarouselItem index={3} />,
-    <CarouselItem index={4} />,
-    <CarouselItem index={5} />,
-  ];
+  type Event = {
+    EventName: string;
+    EventDate: string;
+    EventDoorsOpen: string;
+    ticketTypes: any[];
+    EventUrl: string;
+    EventThumbnail: string;
+    ShowPublic: boolean;
+  };
 
+  const [loading, setLoading] = useState(false);
+  const [events, setEvents] = useState<Event[]>([]);
+
+  function sortEventsByDate(events: Event[]): Event[] {
+    return events.sort((a, b) => {
+      const [yearA, monthA, dayA] = a.EventDate.split("-").map(Number);
+      const [yearB, monthB, dayB] = b.EventDate.split("-").map(Number);
+
+      const dateA = new Date(yearA, monthA - 1, dayA);
+      const dateB = new Date(yearB, monthB - 1, dayB);
+
+      return dateA.getTime() - dateB.getTime();
+    });
+  }
+
+  function getFirstThreeEvents(events: Event[]): Event[] {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return events
+      .filter((event) => {
+        const [year, month, day] = event.EventDate.split("-").map((num) =>
+          parseInt(num, 10)
+        );
+        const eventDate = new Date(year, month - 1, day);
+        return eventDate >= today && event.ShowPublic;
+      })
+      .slice(0, 6); // Only return first 3
+  }
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://67sxoyzalltf7cnmecge2sj5yq0bckqd.lambda-url.us-east-2.on.aws",
+        {
+          method: "GET",
+          headers: { "x-api-key": "7a055rda" },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP ERROR, STATUS: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data && data.length > 0) {
+        const sortedEvents = sortEventsByDate(data);
+        setEvents(getFirstThreeEvents(sortedEvents));
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error("Error fetching event data:", error);
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const carouselItems = events.map((eventItem, index) => (
+    <SwiperSlide key={index} className="swiper-slide-container-class">
+      <CarouselItem
+        mainTitle={eventItem.EventName}
+        subtitle="Get your tickets now!"
+        image={eventItem.EventThumbnail}
+        url={eventItem.EventUrl}
+        buttonText="TICKETS"
+      />
+    </SwiperSlide>
+  ));
   return (
     <Swiper
       effect="coverflow"
@@ -53,11 +126,12 @@ export default function Carousel() {
       }}
       className="swiper-container-class"
     >
-      {carouselItems.map((item, index) => (
+      {/* {carouselItems.map((item, index) => (
         <SwiperSlide key={index} className="swiper-slide-container-class">
           {item}
         </SwiperSlide>
-      ))}
+      ))} */}
+      {carouselItems}
     </Swiper>
   );
 }
--- Haven/src/components/CarouselItem/CarouselItem.tsx
+++ Haven/src/components/CarouselItem/CarouselItem.tsx
@@ -3,22 +3,37 @@ import { useState } from "react";
 import JasonPic from "../../assets/media/haven pics/DSCF7307.jpg";
 
 type CarouselItemProps = {
-  index: number;
+  subtitle: string;
+  url: string;
+  mainTitle: string;
+  image: string;
+  buttonText: string;
 };
-export default function CarouselItem({ index }: CarouselItemProps) {
+export default function CarouselItem({
+  mainTitle,
+  subtitle,
+  url,
+  image,
+  buttonText,
+}: CarouselItemProps) {
   return (
     <div
       className="carousel-item-container"
-      style={{ backgroundImage: `url(${JasonPic})` }}
+      style={{ backgroundImage: `url(${image})` }}
     >
       <div className="carousel-item-title">
-        <h6>Carousel Title</h6>
+        <h6>{mainTitle}</h6>
         <p>
-          <i>{index}</i>
+          <i>{subtitle}</i>
         </p>
       </div>
-      <button className="carousel-item-button">
-        <h6>BUTTON</h6>
+      <button
+        className="carousel-item-button"
+        onClick={() => {
+          window.open(url, "_blank", "noopener,noreferrer");
+        }}
+      >
+        <h6>{buttonText}</h6>
       </button>
     </div>
   );
