451baaa0084f4bb5b370b0eb3c0b643b8d92f731
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-09-30 13:04:13 -0400

    Working Footer links
    links include pg.media, link to google maps, instagram, and facebook

--- Haven/src/components/Footer/Footer.tsx
+++ Haven/src/components/Footer/Footer.tsx
@@ -4,8 +4,7 @@ import {
   FaFacebookF,
   FaDiscord,
   FaInstagram,
-  FaEnvelope,
-  FaI,
+  FaLocationDot,
 } from "react-icons/fa6";
 
 import pgLogo from "../../assets/media/pglogo-white-no-text.png";
@@ -23,16 +22,33 @@ export default function Footer() {
         </div>
         <div className="icon-container">
           <div className="fa-icon">
-            <img src={pgLogo} alt="" />
+            <a href="https://pg.media" target="_blank">
+              <img src={pgLogo} alt="" />
+            </a>
           </div>
           <div className="fa-icon">
-            <FaInstagram size={48} color="white" />
+            <a
+              href="https://www.google.com/maps/dir//401+Atwood+St,+Pittsburgh,+PA+15213/@40.4380792,-80.0360765,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8834f1e595444105:0xbd19f85eed9d81e!2m2!1d-79.9536756!2d40.4381085?entry=ttu&g_ep=EgoyMDI1MDkyNC4wIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+            >
+              <FaLocationDot size={48} color="white" />
+            </a>
           </div>
           <div className="fa-icon">
-            <FaFacebookF size={48} color="white" />
+            <a
+              href="https://www.instagram.com/haven.pgh/?hl=en"
+              target="_blank"
+            >
+              <FaInstagram size={48} color="white" />
+            </a>
           </div>
           <div className="fa-icon">
-            <FaDiscord size={48} color="white" />
+            <a
+              href="https://www.facebook.com/p/HAVEN-Pgh-61576887341038/"
+              target="_blank"
+            >
+              <FaFacebookF size={48} color="white" />
+            </a>
           </div>
         </div>
       </div>
