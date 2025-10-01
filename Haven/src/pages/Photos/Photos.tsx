f13e9669812a82775c98ca0a56f7d6373843afa6
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-10-01 16:59:38 -0400

    added dj nights album

--- Haven/src/pages/Photos/Photos.tsx
+++ Haven/src/pages/Photos/Photos.tsx
@@ -13,13 +13,19 @@ import "swiper/css/scrollbar";
 import { FreeMode, Scrollbar, Mousewheel } from "swiper/modules";
 
 import photos from "./photo-list";
+import djPhotos from "./dj-photo-list";
+import { div } from "motion/react-client";
 
 export default function Photos() {
   //   console.log("photos: ", photos);
   const photosArraySize = 8;
   const splitPhotos = [];
+  const djSplitPhotos = [];
   for (let i = 0; i < photos.length; i += photosArraySize) {
     splitPhotos.push(photos.slice(i, i + photosArraySize));
+  }
+  for (let i = 0; i < djPhotos.length; i += photosArraySize) {
+    djSplitPhotos.push(djPhotos.slice(i, i + photosArraySize));
   }
   console.log("splitPhotos: ", splitPhotos);
 
@@ -37,25 +43,64 @@ export default function Photos() {
     );
   });
 
+  const djPhotoAlbumArray = djSplitPhotos.map((splitPhoto, i) => {
+    return (
+      <SwiperSlide className="photos-swiper-slide">
+        <RowsPhotoAlbum
+          key={i}
+          photos={splitPhoto}
+          //   targetRowHeight={225}
+          rowConstraints={{ minPhotos: splitPhoto.length / 2 }}
+          spacing={20}
+        />
+      </SwiperSlide>
+    );
+  });
+
   return (
     <div className="photos-container">
-      <Swiper
-        direction={"horizontal"}
-        slidesPerView={"auto"}
-        freeMode={true}
-        // scrollbar={{ draggable: true }}
-        // mousewheel={{
-        //   releaseOnEdges: true, // important: allows vertical page scroll at edges
-        //   forceToAxis: false, // allows free vertical scroll even mid-slider
-        //   invert: false, // flip horizontal direction if needed
-        //   sensitivity: 1, // adjust scroll speed
-        // }}
-        spaceBetween={20}
-        modules={[FreeMode, Scrollbar, Mousewheel]}
-        className="swiper-container-class"
-      >
-        {rowPhotoAlbumArray}
-      </Swiper>
+      <h2>LIVE MUSIC</h2>
+      <div className="swiper-photo-album-container">
+        {" "}
+        <Swiper
+          direction={"horizontal"}
+          slidesPerView={"auto"}
+          freeMode={true}
+          // scrollbar={{ draggable: true }}
+          // mousewheel={{
+          //   releaseOnEdges: true, // important: allows vertical page scroll at edges
+          //   forceToAxis: false, // allows free vertical scroll even mid-slider
+          //   invert: false, // flip horizontal direction if needed
+          //   sensitivity: 1, // adjust scroll speed
+          // }}
+          spaceBetween={20}
+          modules={[FreeMode, Scrollbar, Mousewheel]}
+          className="swiper-container-class"
+        >
+          {rowPhotoAlbumArray}
+        </Swiper>
+      </div>
+
+      <h2>DJ NIGHTS</h2>
+      <div className="swiper-photo-album-container">
+        <Swiper
+          direction={"horizontal"}
+          slidesPerView={"auto"}
+          freeMode={true}
+          // scrollbar={{ draggable: true }}
+          // mousewheel={{
+          //   releaseOnEdges: true, // important: allows vertical page scroll at edges
+          //   forceToAxis: false, // allows free vertical scroll even mid-slider
+          //   invert: false, // flip horizontal direction if needed
+          //   sensitivity: 1, // adjust scroll speed
+          // }}
+          spaceBetween={20}
+          modules={[FreeMode, Scrollbar, Mousewheel]}
+          className="swiper-container-class"
+        >
+          {djPhotoAlbumArray}
+        </Swiper>
+      </div>
     </div>
   );
 }
---
+++ Haven/src/pages/Photos/dj-photo-list.ts
@@ -0,0 +1,25 @@
+import type { Photo } from "react-photo-album";
+import photo1 from "../../assets/media/disco nap/IMG_0768.jpg";
+import photo2 from "../../assets/media/disco nap/IMG_1378.jpg";
+import photo3 from "../../assets/media/disco nap/IMG_1640.jpg";
+import photo4 from "../../assets/media/disco nap/IMG_1909.jpg";
+import photo5 from "../../assets/media/disco nap/IMG_2003.jpg";
+import photo6 from "../../assets/media/disco nap/IMG_2149.jpg";
+import photo7 from "../../assets/media/disco nap/IMG_3030.jpg";
+import photo8 from "../../assets/media/disco nap/IMG_3102.jpg";
+
+const djPhotos: Photo[] = [
+  { src: photo1, width: 600, height: 400, alt: "Photo 1" },
+  { src: photo2, width: 600, height: 400, alt: "Photo 2" },
+  { src: photo3, width: 600, height: 400, alt: "Photo 3" },
+  { src: photo4, width: 600, height: 400, alt: "Photo 4" },
+  { src: photo5, width: 600, height: 400, alt: "Photo 5" },
+  { src: photo6, width: 600, height: 400, alt: "Photo 6" },
+  { src: photo7, width: 600, height: 400, alt: "Photo 7" },
+  { src: photo8, width: 600, height: 400, alt: "Photo 8" },
+
+
+
+];
+
+export default djPhotos;
