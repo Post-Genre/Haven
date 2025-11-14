445e0145f763ba21e4bdc49960187660e51009bc
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-11-14 16:44:04 -0500

    all photo albums added but lost ability to swipe

--- Haven/src/pages/Photos/MobilePhotos.tsx
+++ Haven/src/pages/Photos/MobilePhotos.tsx
@@ -7,7 +7,6 @@ import "swiper/css/free-mode";
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
 import { FreeMode, Scrollbar, Mousewheel, Navigation } from "swiper/modules";
-
 import photos from "./photo-list";
 import djPhotos from "./dj-photo-list";
 import obpPhotos from "./obp-photo-list";
@@ -19,7 +18,31 @@ import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 export default function MobilePhotos() {
   const photosSlides = photos.map((photo, i) => {
     return (
-      <SwiperSlide key={i}>
+      <SwiperSlide key={i} className="mobile-photos-swiper-slide">
+        <PhotoSlide
+          title={photo.title}
+          subtitle={photo.alt}
+          image={photo.src}
+        />
+      </SwiperSlide>
+    );
+  });
+
+  const djPhotosSlides = djPhotos.map((photo, i) => {
+    return (
+      <SwiperSlide key={i} className="mobile-photos-swiper-slide">
+        <PhotoSlide
+          title={photo.title}
+          subtitle={photo.alt}
+          image={photo.src}
+        />
+      </SwiperSlide>
+    );
+  });
+
+  const obpPhotosSlides = obpPhotos.map((photo, i) => {
+    return (
+      <SwiperSlide key={i} className="mobile-photos-swiper-slide">
         <PhotoSlide
           title={photo.title}
           subtitle={photo.alt}
@@ -29,24 +52,71 @@ export default function MobilePhotos() {
     );
   });
   return (
-    <div className="mobile-photo-album-container">
-      <div className="custom-prev">
-        <FaAngleLeft color="white" />
+    <div className="mobile-photo-container">
+      <h3>LIVE MUSIC</h3>
+      <div className="mobile-photo-album-container">
+        <div className="custom-prev photos-button">
+          <FaAngleLeft color="white" />
+        </div>
+        <Swiper
+          slidesPerView={1}
+          modules={[FreeMode, Scrollbar, Mousewheel, Navigation]}
+          navigation={{
+            prevEl: ".custom-prev",
+            nextEl: ".custom-next",
+            enabled: true,
+          }}
+          // loop={true}
+        >
+          {photosSlides}
+        </Swiper>
+        <div className="custom-next photos-button">
+          <FaAngleRight color="white" />
+        </div>
       </div>
-      <Swiper
-        slidesPerView={1}
-        modules={[FreeMode, Scrollbar, Mousewheel, Navigation]}
-        navigation={{
-          prevEl: ".custom-prev",
-          nextEl: ".custom-next",
-          enabled: true,
-        }}
-        // loop={true}
-      >
-        {photosSlides}
-      </Swiper>
-      <div className="custom-next">
-        <FaAngleRight color="white" />
+
+      <h3>DJ NIGHTS</h3>
+      <div className="mobile-photo-album-container">
+        <div className="custom-prev photos-button">
+          <FaAngleLeft color="white" />
+        </div>
+        <Swiper
+          slidesPerView={1}
+          modules={[FreeMode, Scrollbar, Mousewheel, Navigation]}
+          navigation={{
+            prevEl: ".custom-prev",
+            nextEl: ".custom-next",
+            enabled: true,
+          }}
+          // loop={true}
+        >
+          {djPhotosSlides}
+        </Swiper>
+        <div className="custom-next photos-button">
+          <FaAngleRight color="white" />
+        </div>
+      </div>
+
+      <h3>OAKLAND BLOCK PARTY 2025</h3>
+      <div className="mobile-photo-album-container">
+        <div className="custom-prev photos-button">
+          <FaAngleLeft color="white" />
+        </div>
+        <Swiper
+          slidesPerView={1}
+          modules={[FreeMode, Scrollbar, Mousewheel, Navigation]}
+          navigation={{
+            prevEl: ".custom-prev",
+            nextEl: ".custom-next",
+            enabled: true,
+          }}
+          // loop={true}
+        >
+          {obpPhotosSlides}
+        </Swiper>
+        <div className="custom-next photos-button">
+          <FaAngleRight color="white" />
+        </div>
       </div>
     </div>
   );
