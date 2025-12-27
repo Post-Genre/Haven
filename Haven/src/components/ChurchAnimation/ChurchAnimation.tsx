25f514b5a5fe3b7942fd3ee3a25b87ff5c692ac8
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-12-26 23:15:54 -0600

    adding scroll animation to HAVEN text
    had to bring back scrollbar to create functionality

--- Haven/src/components/ChurchAnimation/ChurchAnimation.tsx
+++ Haven/src/components/ChurchAnimation/ChurchAnimation.tsx
@@ -3,8 +3,38 @@ import "./church-animation.css";
 import churchLowres from "../../assets/media/backgrounds/Church-Lowres.png";
 import backgroundWrapper from "../../assets/media/backgrounds/psychbackground_lowres.jpg";
 import blackHavenLogo from "../../assets/media/havenlogo-black-updated.png";
+import { useEffect } from "react";
 
 export default function ChurchAnimation() {
+  const scrollItem = scroll;
+
+  function adjustLetterSpacing() {
+    console.log("scroll");
+    let word = document.getElementById("animated-word");
+    if (!word) return;
+    const scrollPosition = window.scrollY;
+
+    const maxSpacing = 40;
+    const startScroll = 0;
+    const endScroll = window.innerHeight * 0.5;
+
+    let letterSpacing = window.innerWidth < 500 ? 0 : 0;
+    if (scrollPosition >= startScroll && scrollPosition <= endScroll) {
+      letterSpacing += (scrollPosition / endScroll) * maxSpacing;
+    } else if (scrollPosition > endScroll) {
+      letterSpacing = maxSpacing;
+    }
+
+    word.style.letterSpacing = `${letterSpacing}px`;
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", adjustLetterSpacing);
+    // document.addEventListener("scroll", adjustLetterSpacing);
+
+    return () => window.removeEventListener("scroll", adjustLetterSpacing);
+  }, []);
+
   return (
     <div className="church-animation-container">
       <img className="church-image" src={churchLowres} alt="Church" />
