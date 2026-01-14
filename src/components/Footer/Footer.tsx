c010ceb118ddffa4012b5b5b75996eb2c35fd4fd
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2026-01-14 13:34:00 -0500

    change uppercase

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import "./footer.css";
 import havenLogo from "../../assets/media/havenlogo-white-updated.png";
 import { FaFacebookF, FaInstagram, FaLocationDot } from "react-icons/fa6";
+//lowercase png
 
 import pgLogo from "../../assets/media/pglogo-white-no-text.png";
 
